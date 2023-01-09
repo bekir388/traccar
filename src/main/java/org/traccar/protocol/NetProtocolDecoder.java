ab38b6cb10ce11f85fc0e23e7e220a64ee1675e8
bekir388 <121623765+bekir388@users.noreply.github.com>
2023-01-09 16:56:08 +0300

    d
    d

--- src/main/java/org/traccar/protocol/NetProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/NetProtocolDecoder.java
@@ -86,7 +86,7 @@ public class NetProtocolDecoder extends BaseProtocolDecoder {
     
         int ignition = parser.nextHexInt();
             if (BitUtil.check(ignition, > 1)) {
-                position.set(Position.KEY_IGNITION, null);
+                position.set(Position.KEY_IGNITION, false);
             } else if (BitUtil.check(ignition, 1)) {
                 position.set(Position.KEY_IGNITION, true);
             }
